576663618b4245a864cfd246a770b6bf57740c03
gggthatch <74699097+gggthatch@users.noreply.github.com>
2024-02-23 08:03:11 +1000

    Add files via upload

---
+++ SDK/chatbot.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from transformers import pipeline
+
+@st.cache(allow_output_mutation=True)
+def get_model():
+    return pipeline('text-generation', model='adlumal/AusLegalQA-Mixtral-8x7B-Instruct-v0.1')
+
+def generate(model, text):
+    response = model(text)
+    return response[0].get('generated_text') if response else 'No response'
+
+st.title('Chat with AI')
+
+usr_input = st.text_input('Your message')
+
+if usr_input:
+    model = get_model()
+    response = generate(model, usr_input)
+    st.markdown(f'*AI*: {response}')
