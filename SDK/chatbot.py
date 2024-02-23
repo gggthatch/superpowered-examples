5d6136568a0e1fc6391305f572285042012accf3
gggthatch <74699097+gggthatch@users.noreply.github.com>
2024-02-23 13:10:48 +1000

    Update chatbot.py

--- SDK/chatbot.py
+++ SDK/chatbot.py
@@ -3,17 +3,17 @@ from transformers import pipeline
 
 @st.cache(allow_output_mutation=True)
 def get_model():
-    return pipeline('text-generation', model='adlumal/AusLegalQA-Mixtral-8x7B-Instruct-v0.1')
+	return pipeline('text-generation', model='umarbutler/open-australian-legal-phi-1_5')
 
 def generate(model, text):
-    response = model(text)
-    return response[0].get('generated_text') if response else 'No response'
+	response = model(text)
+	return response[0].get('generated_text') if response else 'No response'
 
 st.title('Chat with AI')
 
 usr_input = st.text_input('Your message')
 
 if usr_input:
-    model = get_model()
-    response = generate(model, usr_input)
-    st.markdown(f'*AI*: {response}')
+	model = get_model()
+	response = generate(model, usr_input)
+st.markdown(f'*AI*: {response}')
